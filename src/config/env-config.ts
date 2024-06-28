b61a05fddfdcf9a595bcb2e9a590dde4d845bde7
Filipe Salviano <filipesalviano@proton.me>
2024-06-27 23:15:55 -0300

    Propriedade de ambiente: OWNER_ID

--- src/config/env-config.ts
+++ src/config/env-config.ts
@@ -2,13 +2,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { TOKEN, CLIENT_ID } = process.env;
+const { TOKEN, CLIENT_ID, OWNER_ID } = process.env;
 
-if (!CLIENT_ID || !TOKEN) {
+if (!CLIENT_ID || !TOKEN || !OWNER_ID) {
     throw new Error("Sem váriaveis de ambiente necessárias");
 }
 
 export const config = {
     TOKEN,
-    CLIENT_ID
+    CLIENT_ID,
+    OWNER_ID
 };
